a57e594b35c55cb23006c48e1d9680d32ca8fd59
Shal <musthafafaishal@gmail.com>
2024-11-27 14:55:59 +0700

    feat: initialize map layout with leaflet

---
+++ components/organisms/MapSection.jsx
@@ -0,0 +1,10 @@
+import MapContent from '../molecules/MapContent';
+import SectionScreenLayout from '../templates/SectionScreenLayout';
+
+export default function MapSection({}) {
+  return (
+    <SectionScreenLayout>
+      <MapContent />
+    </SectionScreenLayout>
+  );
+}
---
+++ components/templates/SectionScreenLayout.jsx
@@ -0,0 +1,3 @@
+export default function SectionScreenLayout({ children }) {
+  return <section className="w-screen h-dvh">{children}</section>;
+}
